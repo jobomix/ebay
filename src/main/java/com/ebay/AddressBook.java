f7c26e5f07481d65d5da131b74e33082931b65ba
Bruno Abitbol <bruno@stacktraceltd.com>
2015-03-01 14:36:42 +0000

    Added method to retrieve entries and compute difference in days

--- src/main/java/com/ebay/AddressBook.java
+++ src/main/java/com/ebay/AddressBook.java
@@ -4,6 +4,8 @@ import org.joda.time.DateTime;
 import org.joda.time.Days;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.ebay.AddressBookEntry.Gender.Male;
 
@@ -16,9 +18,12 @@ public class AddressBook {
 
     private final AddressBookEntry oldest;
 
+    private final Map<String, AddressBookEntry> entries;
+
     private AddressBook(Builder builder) {
         this.males = builder.males;
         this.oldest = builder.oldest;
+        this.entries = builder.entries;
     }
 
     public int getMales() {
@@ -30,14 +35,34 @@ public class AddressBook {
     }
 
     /**
+     * Retrieve the difference in days.
+     * @param name1 The first entry name
+     * @param name2 The second entry name
+     * @return The difference in days between the 2 entries.
+     */
+    public int getDifferenceInDays(String name1, String name2) {
+        AddressBookEntry entry1 = entries.get(name1);
+        if (entry1 == null) {
+            throw new IllegalArgumentException("The first argument must be an existing name");
+        }
+
+        AddressBookEntry entry2 = entries.get(name2);
+        if (entry2 == null) {
+            throw new IllegalArgumentException("The second argument must be an existing name");
+        }
+        return computeDifferenceInDays(entry1.getDateOfBirth(), entry2.getDateOfBirth());
+    }
+
+    /**
      * Compute the difference in days between 2 DateTime.
      * A positive number means dateTime1 is older than dateTime2
      * A negative number means dateTime1 is younger than dateTime2
+     *
      * @param dateTime1
      * @param dateTime2
      * @return a positive or negative int representing the difference in days for to entries.
      */
-     static int getDifferenceInDays(DateTime dateTime1, DateTime dateTime2) {
+    static int computeDifferenceInDays(DateTime dateTime1, DateTime dateTime2) {
         return Days.daysBetween(dateTime1, dateTime2).getDays();
     }
 
@@ -51,6 +76,8 @@ public class AddressBook {
         private int males = 0;
 
         private AddressBookEntry oldest;
+
+        private Map<String, AddressBookEntry> entries = new HashMap<String, AddressBookEntry>();
 
         public AddressBook build() {
             BufferedReader reader = null;
@@ -67,6 +94,9 @@ public class AddressBook {
                     if (oldest == null || entry.getDateOfBirth().isBefore(oldest.getDateOfBirth())) {
                         oldest = entry;
                     }
+
+                    entries.put(entry.getName(), entry);
+
                 }
             } catch (Exception e) {
                 throw new RuntimeException("Problem occurred while reading input file: ", e);
--- src/test/java/com/ebay/AddressBookTest.java
+++ src/test/java/com/ebay/AddressBookTest.java
@@ -1,14 +1,11 @@
 package com.ebay;
 
 import org.joda.time.DateTime;
-import org.joda.time.Days;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AddressBookTest {
-
 
     @Test
     public void shouldCountTheNumberOfMales() {
@@ -24,18 +21,34 @@ public class AddressBookTest {
 
     @Test
     public void shouldCalculateTheDifferenceInDaysForTwoEntries() {
-        DateTime me = new DateTime(1971,4,30,0,0);
-        DateTime someoneElse = new DateTime(1971,5,3,0,0);
-        assertEquals(3, AddressBook.getDifferenceInDays(me, someoneElse));
-
+        DateTime me = new DateTime(1971, 4, 30, 0, 0);
+        DateTime someoneElse = new DateTime(1971, 5, 3, 0, 0);
+        assertEquals(3, AddressBook.computeDifferenceInDays(me, someoneElse));
     }
 
     @Test
     public void shouldCalculateDiffInDaysReturnANegativeNumberIfFirstEntryIsYounger() {
-        DateTime me = new DateTime(1971,4,30,0,0);
-        DateTime someoneElse = new DateTime(1971,5,3,0,0);
-        assertEquals(-3, AddressBook.getDifferenceInDays(someoneElse, me));
+        DateTime me = new DateTime(1971, 4, 30, 0, 0);
+        DateTime someoneElse = new DateTime(1971, 5, 3, 0, 0);
+        assertEquals(-3, AddressBook.computeDifferenceInDays(someoneElse, me));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldRetrieveTheFirstEntryOrThrowAnException() {
+        AddressBook addressBook = new AddressBook.Builder().build();
+        addressBook.getDifferenceInDays("Not exist", "Sarah Stone");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldRetrieveTheSecondEntryOrThrowAnException() {
+        AddressBook addressBook = new AddressBook.Builder().build();
+        addressBook.getDifferenceInDays("Bill McKnight", "Not exist");
+    }
+
+    @Test
+    public void shouldRetrieveTheDifferenceInDaysBetweenTwoEntries() {
+        AddressBook addressBook = new AddressBook.Builder().build();
+        assertEquals(2862, addressBook.getDifferenceInDays("Bill McKnight", "Paul Robinson"));
+    }
 
 }
